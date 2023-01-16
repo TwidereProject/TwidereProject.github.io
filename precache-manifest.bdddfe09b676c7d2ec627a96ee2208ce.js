77e2817e1dd7da7db79167a9e413fd7d73ec4fd3
Tlaster <Tlaster@users.noreply.github.com>
2023-01-16 03:05:32 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.bdddfe09b676c7d2ec627a96ee2208ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ca6e64fd0db11c5e7eb2fefd6de1c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e57d37194373e1410b",
+    "url": "/static/css/main.51b357f9.chunk.css"
+  },
+  {
+    "revision": "980378abb0e72b79a288",
+    "url": "/static/js/2.3e7587f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e7587f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e57d37194373e1410b",
+    "url": "/static/js/main.33e2b448.chunk.js"
+  },
+  {
+    "revision": "4779e64865f3ed27e9de",
+    "url": "/static/js/runtime-main.f6431b98.js"
+  }
+]);
